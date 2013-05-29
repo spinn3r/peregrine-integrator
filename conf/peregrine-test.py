603539ed91367bef5d4959ffadf6eec9b9831ac1
burtonator <devnull@localhost>
2013-05-29 10:28:45 -0700

    age

--- conf/peregrine-test.py
+++ conf/peregrine-test.py
@@ -11,3 +11,5 @@ TEST_COMMAND="export ANT_OPTS=-Xmx512M && time ant test"
 REPO="https://burtonator:redapplekittycat@bitbucket.org/burtonator/peregrine"
 
 OUTPUT = { 'test-reports' : 'target/test-reports'  }
+
+OLD_AGE=3 * 60 * 60
