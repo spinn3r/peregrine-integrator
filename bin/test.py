09a6eb135389bec75cd856c8288c378563ffa054
burtonator <devnull@localhost>
2013-06-07 13:42:33 -0700

    ...

---
+++ bin/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import datetime
+import os
+import re
+import shutil
+import subprocess
+import sys
+import time
+import traceback
+import libintegrator
+
+# from string import Template
+
+# title = "hello world"
+
+# tmpl=Template( """
+# <html>
+# <head>
+# <title>${title}</title>
+# <body>
+
+# </body>
+
+# """ )
+
+# html=tmpl.substitute( title=title )
+
+# print html
+
+
+print libintegrator.get_rev_status( "/var/lib/integration/peregrine/test-1_1_2_3", 3605 )
+print libintegrator.get_rev_status( "/var/lib/integration/peregrine/test-1_1_2_3", 3591 )
